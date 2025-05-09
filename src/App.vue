6b4658e7ad69dc500b441daaef28d2d1c559687e
Thinkedou <r.enguehard@gmail.com>
2025-05-09 17:04:28 +0200

    first

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+  import {ref} from 'vue'
+  import MemberCard from './components/MemberCard.vue'
+  import { TEAMS } from './assets/js/teamsApp';
+
+  const localMember = ref(TEAMS)
+
+console.log(localMember)
+</script>
+
+<template>
+  <MemberCard 
+    v-for="member in localMember"
+    :key="member.profilePic"
+    :member="member"
+  />
+</template>
+
+<style scoped>
+</style>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import './assets/css/card_styles.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
